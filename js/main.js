4bb1cbd94efae9e93d4c6c1c668b80606651254d
Miguel <fedosieiev.mihel@gmail.com>
2021-09-28 16:31:21 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function randomNumber(min, max, afterPoint) {
-  if (min >= 0 && max >= 0 && max >= 0) {
+  if (min >= 0 && max >= 0 && afterPoint >= 0) {
     let rand = min + (Math.random() * (max - min));
     return Number(rand.toFixed(afterPoint));
   }
