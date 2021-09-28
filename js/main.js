0def54218cc7791483dcb232d46b90c7247c0615
Miguel <fedosieiev.mihel@gmail.com>
2021-09-28 16:27:32 +0300

    random number

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function randomNumber(min, max, afterPoint) {
+  if (min >= 0 && max >= 0) {
+    let rand = min + (Math.random() * (max - min));
+    return Number(rand.toFixed(afterPoint));
+  }
+
+  alert('The range can only be positive, including zero!')
+}
+
+randomNumber(0, 10, 5);
