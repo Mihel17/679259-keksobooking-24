daecb8cbbe1d232754a55f6f20e371f9655da2e7
Miguel <fedosieiev.mihel@gmail.com>
2021-09-28 16:39:49 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function randomNumber(min, max, afterPoint) {
   if (min >= 0 && max >= 0 && afterPoint >= 0) {
-    let rand = min + (Math.random() * (max - min));
+    const rand = min + (Math.random() * (max - min));
     return Number(rand.toFixed(afterPoint));
   }
 
-  alert('The range can only be positive, including zero!')
+  console.log('The range can only be positive, including zero!');
 }
 
 randomNumber(0, 10, 0);
