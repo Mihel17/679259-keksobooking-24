93bd13d22a3a1e88ffdbb62b26ce7780c13cb0da
Miguel <fedosieiev.mihel@gmail.com>
2021-10-01 18:08:35 +0300

    first function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function randomInt(min, max) {
+  if (min >= 0 && max >= 0) {
+    const rand = Math.round(min + (Math.random() * (max - min)));
+    return rand;
+  }
+
+  return 'The range can only be positive, including zero!';
+}
+
+randomInt(0, 10);
+
 function randomNumber(min, max, afterPoint) {
   if (min >= 0 && max >= 0 && afterPoint >= 0) {
     const rand = min + (Math.random() * (max - min));
