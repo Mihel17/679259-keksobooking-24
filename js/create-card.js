9b81e5baff8b091e989ae5cac5bce4b3c4856696
Miguel <fedosieiev.mihel@gmail.com>
2021-10-23 20:15:44 +0300

    template avatar

--- js/create-card.js
+++ js/create-card.js
@@ -1,40 +1,73 @@
 import { offers } from './data.js';
 
-// const TYPE_TRANSLATE_OBJECT = {
-//   flat: 'Квартира',
-//   bungalow: 'Бунгало',
-//   house: 'Дом',
-//   palace: 'Дворец',
-//   hotel: 'Отель',
-// };
-
-// TYPE_TRANSLATE_OBJECT.keys(TYPE_TRANSLATE_ARRAY);
-// console.log(TYPE_TRANSLATE_ARRAY);
-
-// const traslateType = (type) => {
-//   TYPE_TRANSLATE.forEach((typeItem) => {
-//     if (type === typeItem) {
-//       type.offer.type = typeItem;
-//       console.log(type.offer.type);
-//     }
-//   });
-// };
+const TYPE_TRANSLATE = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
 
 console.log(offers);
+
+// console.log(collectionItem);
+// console.log(necessaryElements);
+//       // necessaryElements.some(
+//       //   (necessaryElement) => {
+//       //     collectionItem.classList.contains(`popup__feature--${necessaryElement}`);
+//       //   });
+
+
+const deleteUnnecessaryElements = (containerClass, necessaryElements) => {
+  const functionMy = () => {
+    const containers = document.querySelectorAll(containerClass);
+    containers.forEach((container) => {
+      const collectionItem = container.children;
+      const isNecessary = necessaryElements.some(
+        (necessaryElement) => collectionItem.classList.contains(`popup__feature--${necessaryElement}`),
+      );
+      if (!isNecessary) {
+        collectionItem.remove();
+      }
+    });
+  };
+  return functionMy;
+};
+
+// emojiList.forEach((emojiListItem) => {
+//   const isNecessary = userEmotions.some(
+//     (userEmotion) => emojiListItem.classList.contains('emoji--' + userEmotion),
+//   );
+//   if (!isNecessary) {
+//     emojiListItem.remove();
+//   }
+// });
+
 
 const mapContainer = document.querySelector('#map-canvas');
 const CARD_TEMPLATE = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-const createCard = offers.forEach((offer) => {
-  const card = CARD_TEMPLATE.cloneNode(true);
-  card.querySelector('.popup__title').textContent = offer.offer.title;
-  card.querySelector('.popup__text--address').textContent = offer.offer.address;
-  card.querySelector('.popup__text--price').textContent = `${offer.offer.price}$`;
-  // card.querySelector('.popup__type').textContent = traslateType(offer.offer.type);
-  // add to html
-  mapContainer.append(card);
-});
+const createCard = (array) => {
+  array.forEach((offer) => {
+    const card = CARD_TEMPLATE.cloneNode(true);
+    card.querySelector('.popup__title').textContent = offer.offer.title;
+    card.querySelector('.popup__text--address').textContent = offer.offer.address;
+    card.querySelector('.popup__text--price').textContent = `${offer.offer.price}$`;
+    card.querySelector('.popup__type').textContent = TYPE_TRANSLATE[offer.offer.type];
+    card.querySelector('.popup__text--capacity').textContent = `${offer.offer.rooms} комнаты для ${offer.offer.guests} гостей`;// make funct "1 гостей"
+    card.querySelector('.popup__text--time').textContent = `Заезд после ${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
+    // deleteUnnecessaryElements('.popup__features', offer.offer.features);
+    deleteUnnecessaryElements('.popup__features', offer.offer.features);
+    card.querySelector('.popup__description').textContent = offer.offer.description;
+    card.querySelector('.popup__photo').src = offer.offer.photos;
+    card.querySelector('.popup__avatar').src = offer.author.avatar;
 
-createCard();
+
+    // add to html
+    mapContainer.append(card);
+  });
+};
+
+createCard(offers);
--- js/data.js
+++ js/data.js
@@ -1,6 +1,20 @@
 import { randomNumber, randomFromArray, shuffle } from './utils/get-random.js';
 
 const OFFERS_COUNT = 10;
+const TITLES = ['Vista Sunrise Apartments', 'CASA BELLA', 'Chaparral Apartments', '8th And Wake', 'Casa Bonita', 'Villa Anaheim', '238 Termino', 'Artthaus Studios', 'Reflections At Wyandotte', 'Rosewood Park'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const CHECKING_TIME = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+const DESCRIPTIONS = [
+  'Cozy and spacious studio with sea view on the 15th floor of the residential complex YALÇIN STAR RESIDENCE, located on Pirosmani street in an area with developed infrastructure. On the ground floor of the complex there is a supermarket YALÇIN MARKET.From the balcony is beautiful view of the sea and the city. Walking distance to sea and beach, aquapark and the seaside Boulevard, singing fountains, cafes and restaurants.',
+  'Spacious 1 bedroom studio in Batumi is located in the complex"Real Palace". Flat with sea and mountain views is located on the 24th floor of new building on Pirosmani street. The apartment has two rooms: living room-studio (kitchen, dining room and living room) and one spacious bedroom, in addition entrance hall, bathroom and balcony with sea and mountain views. It offers four beds: double bed in the bedroom and sofa bed for two people.There are all conditions for comfortable stay, Internet, TV, all necessary appliances: refrigerator, washing machine, microwave, electric kettle, iron and Ironing Board, air conditioning winter - summer, gas heating. Near the house there are two supermarkets, Yalchin Market and WILLMART, bus stop and taxi rank, within walking distance – the sea and the beach, water Park and seaside Boulevard with singing fountains, cafes and restaurants.Buses run along the Boulevard and the coast, so you can not get lost.The city center can be reached in 5 - 7 minutes or on foot in 15 - 20 minutes.',
+  'The presence of numerous amenities in this apartment will allow guests to feel at home: 1. air conditioning and washing machine. 2. fridge and stove, 3. TV and wireless internet(Wi - Fi), 4. microwave and electric kettle, 5. bed linen towels, hair dryer and iron. From the 26th floor offers stunning views of the sea and the city.Near the house there are two supermarkets, Yalchin Market and WILLMART, bus stop and taxi rank, within walking distance - the sea and the beach, water park and Primorsky Boulevard with singing fountains, cafes and restaurants. Buses run along the boulevard and the coast, so it’s impossible to get lost.The city center can be reached in 7 - 10 minutes or on foot in 20 - 30 minutes.',
+  'Cozy bright studio with sea views on the 24th floor of the Real Palace residential complex, located on ul. Pirosmani in an area with developed infrastructure. The apartment can accommodate no more than 3 adult guests or two adults and one child: a bed for two people and a folding chair for one. Bedding, towels and kitchenware are provided.A full - fledged household and technical equipment of this apartment will allow guests to feel comfortable.The apartment has air conditioning, washing machine, refrigerator and microwave, TV and Wi - Fi, electric kettle, hairdryer and iron.',
+];
 
 const Price = {
   MIN: 0,
@@ -29,28 +43,12 @@ const Guests = {
   MAX: Rooms.MAX * 2,
 }
 
-const TITLES = ['Vista Sunrise Apartments', 'CASA BELLA', 'Chaparral Apartments', '8th And Wake', 'Casa Bonita', 'Villa Anaheim', '238 Termino', 'Artthaus Studios', 'Reflections At Wyandotte', 'Rosewood Park'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const CHECKING_TIME = ['12:00', '13:00', '14:00'];
-const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
-const DESCRIPTIONS = [
-  'Cozy and spacious studio with sea view on the 15th floor of the residential complex YALÇIN STAR RESIDENCE, located on Pirosmani street in an area with developed infrastructure. On the ground floor of the complex there is a supermarket YALÇIN MARKET.From the balcony is beautiful view of the sea and the city. Walking distance to sea and beach, aquapark and the seaside Boulevard, singing fountains, cafes and restaurants.',
-  'Spacious 1 bedroom studio in Batumi is located in the complex"Real Palace". Flat with sea and mountain views is located on the 24th floor of new building on Pirosmani street. The apartment has two rooms: living room-studio (kitchen, dining room and living room) and one spacious bedroom, in addition entrance hall, bathroom and balcony with sea and mountain views. It offers four beds: double bed in the bedroom and sofa bed for two people.There are all conditions for comfortable stay, Internet, TV, all necessary appliances: refrigerator, washing machine, microwave, electric kettle, iron and Ironing Board, air conditioning winter - summer, gas heating. Near the house there are two supermarkets, Yalchin Market and WILLMART, bus stop and taxi rank, within walking distance – the sea and the beach, water Park and seaside Boulevard with singing fountains, cafes and restaurants.Buses run along the Boulevard and the coast, so you can not get lost.The city center can be reached in 5 - 7 minutes or on foot in 15 - 20 minutes.',
-  'The presence of numerous amenities in this apartment will allow guests to feel at home: 1. air conditioning and washing machine. 2. fridge and stove, 3. TV and wireless internet(Wi - Fi), 4. microwave and electric kettle, 5. bed linen towels, hair dryer and iron. From the 26th floor offers stunning views of the sea and the city.Near the house there are two supermarkets, Yalchin Market and WILLMART, bus stop and taxi rank, within walking distance - the sea and the beach, water park and Primorsky Boulevard with singing fountains, cafes and restaurants. Buses run along the boulevard and the coast, so it’s impossible to get lost.The city center can be reached in 7 - 10 minutes or on foot in 20 - 30 minutes.',
-  'Cozy bright studio with sea views on the 24th floor of the Real Palace residential complex, located on ul. Pirosmani in an area with developed infrastructure. The apartment can accommodate no more than 3 adult guests or two adults and one child: a bed for two people and a folding chair for one. Bedding, towels and kitchenware are provided.A full - fledged household and technical equipment of this apartment will allow guests to feel comfortable.The apartment has air conditioning, washing machine, refrigerator and microwave, TV and Wi - Fi, electric kettle, hairdryer and iron.',
-];
-
 const offers = [];
-
 const addOffers = () => {
   for (let i = 0; i < OFFERS_COUNT; i++) {
     offers.push({
       author: {
-        avatar: `img/avatars/user${i + 1 < 10 ? `0${i}` : `''${i}`}.png`,
+        avatar: `img/avatars/user${i + 1 < 10 ? `0${i + 1}` : `${i + 1}`}.png`,
       },
       offer: {
         title: randomFromArray(TITLES),
@@ -59,8 +57,8 @@ const addOffers = () => {
         type: randomFromArray(TYPES),
         rooms: randomNumber(Rooms.MIN, Rooms.MAX),
         guests: randomNumber(Guests.MIN, Guests.MAX),
-        checking_TIME: randomFromArray(CHECKING_TIME),
-        checkout_TIME: randomFromArray(CHECKOUT_TIME),
+        checkin: randomFromArray(CHECKING_TIME),
+        checkout: randomFromArray(CHECKOUT_TIME),
         features: shuffle(FEATURES).slice(0, randomNumber(0, FEATURES.length)),
         description: randomFromArray(DESCRIPTIONS),
         photos: randomFromArray(PHOTOS),
