37e464ee875d269ed695ae29ac9542c90badeb63
Miguel <fedosieiev.mihel@gmail.com>
2021-10-25 14:51:39 +0300

    block form

---
+++ js/form.js
@@ -0,0 +1,26 @@
+const FORM_ADD = document.querySelector('.ad-form');
+const FILTER = document.querySelector('.map__filters');
+
+function lockForm(boolean, ...forms) {
+  if (boolean) {
+    for (const form of forms) {
+      form.classList.add(`${form.classList[0]}--disabled`);
+      const formElements = form.children;
+      for (const item of formElements) {
+        item.disabled = true;
+      }
+    }
+  } else {
+    for (const form of forms) {
+      form.classList.remove(`${form.classList[0]}--disabled`);
+      const formElements = form.children;
+      for (const item of formElements) {
+        item.disabled = false;
+      }
+    }
+  }
+}
+
+lockForm(1, FILTER, FORM_ADD);
+
+
