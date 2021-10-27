ffcd4de82785669c342b2c0c30019a5440a87046
Miguel <fedosieiev.mihel@gmail.com>
2021-10-27 11:03:03 +0300

    questions

--- js/locker-page.js
+++ js/locker-page.js
@@ -7,6 +7,13 @@ const filter = document.querySelector('.map__filters');
 const lockForm = (boolean, ...forms) => {
   if (boolean) {
     mapContainer.innerHTML = '';
+
+    // будут дубликаты, так как в ветке елсе отрисовуем карточки при каждой разблокировке страницы. Решение отрисовывать только 1-ин раз, во время загрузки страницы, в не зависимости от состояния (заблокирована либо нет). И показывать и скрывать карту с помощью display block
+
+    // for (const item of mapContainer.children) {
+    //   item.style.display = 'none';
+    // }
+
     for (const form of forms) {
       form.classList.add(`${form.classList[0]}--disabled`);
       const formElements = form.children;
