31bb841c889c082463ed73547cd858c9794760ea
Miguel <fedosieiev.mihel@gmail.com>
2021-10-28 19:55:22 +0300

    checking checkout

--- js/form.js
+++ js/form.js
@@ -6,6 +6,10 @@ const appartmentType = formAdd.querySelector('#type');
 const roomNumber = formAdd.querySelector('#room_number');
 const capacity = Array.from(document.querySelector('#capacity').children);
 const disabledFilelds = document.querySelectorAll('fieldset, select.map__filter');
+const timeIn = formAdd.querySelector('#timein');
+const timeInOptions = Array.from(formAdd.querySelector('#timein').children);
+const timeOut = formAdd.querySelector('#timeout');
+const timeOutOptions = Array.from(formAdd.querySelector('#timeout').children);
 
 const AppartmentType = {
   bungalow: 0,
@@ -20,6 +24,18 @@ const RoomsNumber = {
   2: [1, 2],
   3: [1, 2, 3],
   100: [0],
+};
+
+const TimeIn = {
+  '12:00': ['12:00'],
+  '13:00': ['12:00', '13:00'],
+  '14:00': ['12:00', '13:00', '14:00'],
+};
+
+const TimeOut = {
+  '12:00': ['12:00', '13:00', '14:00'],
+  '13:00': ['12:00', '13:00'],
+  '14:00': ['12:00'],
 };
 
 capacity
@@ -40,6 +56,34 @@ const onRoomNumberChage = (evt) => {
   });
 };
 roomNumber.addEventListener('change', onRoomNumberChage);
+
+const onTimeInChange = (evt) => {
+  const valueIn = TimeIn[evt.target.value];
+  timeOutOptions.forEach((option) => {
+    const isNecessary = valueIn.some((necessaryValue) => option.value === necessaryValue ? 1 : 0);
+    if (!isNecessary) {
+      option.style.display = 'none';
+    } else {
+      option.style.display = 'block';
+      option.selected = true;
+    }
+  });
+};
+timeIn.addEventListener('change', onTimeInChange);
+
+const onTimeOutChange = (evt) => {
+  const valueOut = TimeOut[evt.target.value];
+  timeInOptions.forEach((option) => {
+    const isNecessary = valueOut.some((necessaryValue) => option.value === necessaryValue ? 1 : 0);
+    if (!isNecessary) {
+      option.style.display = 'none';
+    } else {
+      option.style.display = 'block';
+      option.selected = true;
+    }
+  });
+};
+timeOut.addEventListener('change', onTimeOutChange);
 
 const onAppartmentTypeChange = (evt) => {
   const minPrice = AppartmentType[evt.target.value];
