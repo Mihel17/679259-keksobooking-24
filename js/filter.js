2cb5c398765c22304083acbc0b57f25561641a92
Miguel <fedosieiev.mihel@gmail.com>
2021-11-14 22:44:19 +0200

    filter

--- js/filter.js
+++ js/filter.js
@@ -18,29 +18,11 @@ const priceMap = {
 const filters = Array.from(document.querySelector('.map__filters').children);
 
 
-// const filterRules = {
-//   'housing-type': (data, filter) => {
-//     console.log(filter.value === data.offer.type);
-//     filter.value === data.offer.type;
-//   },
-//   'housing-price': (data, filter) =>
-//     (priceMap[filter.value].FROM === data.offer.price && priceMap[filter.value].TO > data.offer.price),
-//   'housing-rooms': (data, filter) => {
-//     console.log(filter.value === String(data.offer.rooms));
-//     filter.value === String(data.offer.rooms);
-//   },
-//   'housing-guests': (data, filter) => filter.value === String(data.offer.guests),
-//   'housing-features': (data, filter) => {
-//     const activeCheckboxes = Array.from(filter.querySelectorAll('input[type="checkbox"]:checked'));
-
-//     return activeCheckboxes.every((checkbox) =>
-//       (data.offer.features.some((feature) => feature === checkbox.value)));
-//   },
-// };
-
-
 const filterRules = {
-  'housing-type': (data, filter) => filter.value === data.offer.type,
+  'housing-type': (data, filter) => {
+    console.log(filter.value, data.offer.type, filter.value === data.offer.type);
+    filter.value === data.offer.type;
+  },
   'housing-price': (data, filter) =>
     (priceMap[filter.value].FROM === data.offer.price && priceMap[filter.value].TO > data.offer.price),
   'housing-rooms': (data, filter) => filter.value === String(data.offer.rooms),
@@ -56,8 +38,8 @@ const filterRules = {
 const filterData = (data) => {
   const filterOffers = [];
 
-  for (let i = 0; filterOffers.length < MAX_OFFERS; i++) {
-    const result = filters.filter((filter) => {
+  for (let i = 0; i < data.length && filterOffers.length < MAX_OFFERS; i++) {
+    const result = filters.every((filter) => {
 
       //=================
       console.log(i);
